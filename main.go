e0ce35c1b9f34c156515276e04d01dfb38b4aee9
Cosmos307 <156374667+Cosmos307@users.noreply.github.com>
2025-05-22 14:41:12 +0200

    feat: improve graph output and add output for Dijkstra results

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/Cosmos307/graphenalgorithmen/graph"
+import (
+	"fmt"
+
+	"github.com/Cosmos307/graphenalgorithmen/algorithm"
+	"github.com/Cosmos307/graphenalgorithmen/graph"
+)
 
 func main() {
-	graph.NewRandomGraph(30, 0.3, 0, 10).Print()
+	g := graph.NewRandomGraph(10, 0.3, 1, 10)
+	fmt.Println("--- Generierter Graph ---")
+	g.Print()
+
+	dist, prev := algorithm.Dijkstra(g, 0)
+	fmt.Println("\n--- Dijkstra (Startknoten 0) ---")
+	for node := 0; node < g.Nodes; node++ {
+		fmt.Printf("Knoten %d: Distanz = %d, Vorgänger = %d\n", node, dist[node], prev[node])
+	}
 }
