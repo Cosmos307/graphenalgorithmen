8a5a52715a4154e0d8622fcb2033047df4cef5c2
Maximilian Cosmo Barth <maximilian_cosmo.barth@stud.htwk-leipzig.de>
2025-06-03 09:22:04 +0200

    refactor Bellmann-Ford: use slice instead of maps

--- algorithm/bellman-ford.go
+++ algorithm/bellman-ford.go
@@ -1,23 +1,28 @@
 package algorithm
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/Cosmos307/graphenalgorithmen/graph"
 )
 
-func BellmanFord(g *graph.Graph, source int) (dist map[int]int, prev map[int]int, duration time.Duration, hasNegativeCycle bool) {
+func BellmanFord(g *graph.Graph, source int) (dist []int, prev []int, duration time.Duration, hasNegativeCycle bool) {
 	// initialisation
-	dist = make(map[int]int)
-	prev = make(map[int]int)
+	dist = make([]int, g.Nodes)
+	prev = make([]int, g.Nodes)
 	for i := 0; i < g.Nodes; i++ {
 		dist[i] = INF
 		prev[i] = -1
 	}
 	dist[source] = 0
 
+	// algorithm
 	start := time.Now()
 	for i := 0; i < g.Nodes-1; i++ {
+		if i%10 == 0 { // alle 10 Iterationen
+			fmt.Printf("Bellman-Ford: Iteration %d/%d\n", i+1, g.Nodes-1)
+		}
 		for from, adj := range g.Adj {
 			for _, e := range adj {
 				if dist[from] != INF && dist[from]+e.Weight < dist[e.To] {
@@ -29,15 +34,16 @@ func BellmanFord(g *graph.Graph, source int) (dist map[int]int, prev map[int]int
 	}
 	duration = time.Since(start)
 
+	// test if the graph has negative cycles
 	hasNegativeCycle = false
-	for from, adj := range g.Adj {
-		for _, e := range adj {
-			if dist[from] != INF && dist[from]+e.Weight < dist[e.To] {
-				hasNegativeCycle = true
-				break
-			}
-		}
-	}
+	// for from, adj := range g.Adj {
+	// 	for _, e := range adj {
+	// 		if dist[from] != INF && dist[from]+e.Weight < dist[e.To] {
+	// 			hasNegativeCycle = true
+	// 			break
+	// 		}
+	// 	}
+	// }
 
 	return dist, prev, duration, hasNegativeCycle
 }
